a17e4cd2ff85b4ac03713d51ceded2e4b55dc103
Folarin Ogungbemi <Folarine1@gmail.com>
2022-09-13 13:19:12 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,11 +10,13 @@ document.addEventListener("DOMContentLoaded", function(){
                 alert("You clicked Submit!");
             }else{
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
+
             }
-        })
-        
+        })  
     }
+
+    runGame("addition");
 })
 
 //Doc strings at the top of function to exlplain what the function intend to do.
@@ -23,11 +25,19 @@ document.addEventListener("DOMContentLoaded", function(){
  * and after the user's answer has been processed
  */
 
-function runGame(){
+function runGame(gameType){
 
     // creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition"){
+        displayAdditionQuestion(num1, num2);
+    }
+    else{
+        alert(`Unknown game type: ${gameType}`);
+        throw (`Unknown game type: ${gameType}.Aborting!`);
+    }
 }
 
 
@@ -47,8 +57,10 @@ function incrementWrongAnswer(){
 
 }
 
-function displayAdditionQuestion(){
-
+function displayAdditionQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion(){
